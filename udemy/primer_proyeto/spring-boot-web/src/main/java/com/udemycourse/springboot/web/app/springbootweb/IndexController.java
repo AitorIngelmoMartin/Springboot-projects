e36c1574d26ac5ea38ca4f97066d4f4edcd13923
GabrielExt <aitorete2000@gmail.com>
2023-08-02 19:09:26 +0200

    Agregado model.addAttribute para modificar la vista

--- udemy/primer_proyeto/spring-boot-web/src/main/java/com/udemycourse/springboot/web/app/springbootweb/IndexController.java
+++ udemy/primer_proyeto/spring-boot-web/src/main/java/com/udemycourse/springboot/web/app/springbootweb/IndexController.java
@@ -1,15 +1,32 @@
 package com.udemycourse.springboot.web.app.springbootweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
     //Los metodos siempre van a ser publicos
     
     //@RequestMapping(value = '/index',method=RequestMethod.GET) //Es GET por defecto, pero podemso especificar
-    @GetMapping(value = {"/index","/home"})//Esta opción es igual pero especificada
-    public String index(){
+    @GetMapping(value = {"/index","","/","/home"})//Esta opción es igual pero especificada
+    public String index(Model model){
+        model.addAttribute("titulo","Titulo pasado por model");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model){
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Aitor");
+        usuario.setApellido("Ingelmo");
+
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("titulo","Perfil de: ".concat(usuario.getNombre()));
+
+        return "perfil";
     }
 }
