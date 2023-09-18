54ccf287973b3cac9fa37b66744ea66707ab878f
GabrielExt <aitorete2000@gmail.com>
2023-09-18 15:35:16 +0200

    Agregado ModelAttribute para pasar la lista de suarios a todas las vistas dentro de /app

--- udemy/primer_proyeto/spring-boot-web/src/main/java/com/udemycourse/springboot/web/app/springbootweb/IndexController.java
+++ udemy/primer_proyeto/spring-boot-web/src/main/java/com/udemycourse/springboot/web/app/springbootweb/IndexController.java
@@ -1,18 +1,19 @@
 package com.udemycourse.springboot.web.app.springbootweb;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/app") //Esta es la ruta raíz. A partir de esta se accede a las demás
 public class IndexController {
     //Los metodos siempre van a ser publicos
-    
     //@RequestMapping(value = '/index',method=RequestMethod.GET) //Es GET por defecto, pero podemos especificar
     @GetMapping(value = {"/index","","/","/home"})//Esta opción es igual pero especificada
     public String index(Model model){
@@ -22,8 +23,8 @@ public class IndexController {
 
     @RequestMapping("/perfil")
     public String perfil(Model model){
-
         Usuario usuario = new Usuario();
+
         usuario.setNombre("Aitor");
         usuario.setApellido("Ingelmo");
         usuario.setEmail("mail_aitor@email.com");
@@ -36,15 +37,20 @@ public class IndexController {
 
     @RequestMapping("/listar")
     public String listar(Model model){
-
-        List<Usuario> usuarios = new ArrayList<>();
-        
-        usuarios.add(new Usuario("Aitor","Ingelmo","mail_aitor@email.com"));
-        usuarios.add(new Usuario("Samuel","Navarro","mail_samuel@email.com"));
-        usuarios.add(new Usuario("Noelia","Dafos","mail_noelia@email.com"));
         model.addAttribute("titulo","Listado de usuarios");
-        model.addAttribute("usuarios",usuarios);
 
         return "listar";
+    }
+
+    //Esta es otra forma de pasar datos a todas las vistas. Debemos pasar la variable en el '@', el metodo retorna el valor
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios(){
+        List<Usuario> usuarios = Arrays.asList(
+            new Usuario("Aitor","Ingelmo","mail_aitor@email.com"),
+            new Usuario("Samuel","Navarro","mail_samuel@email.com"),
+            new Usuario("Noelia","Dafos","mail_noelia@email.com")
+            );
+
+        return usuarios;
     }
 }
