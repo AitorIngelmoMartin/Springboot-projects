365ada46b76849a4f7a38a91b2fca72fdcaf2f7b
GabrielExt <aitorete2000@gmail.com>
2023-09-18 17:33:27 +0200

    Agregada otra forma de pasar pa´rametros por cabecera vía 'HttpServletRequest'.

--- udemy/primer_proyeto/spring-boot-web/src/main/java/com/udemycourse/springboot/web/app/springbootweb/EjemploParamsController.java
+++ udemy/primer_proyeto/spring-boot-web/src/main/java/com/udemycourse/springboot/web/app/springbootweb/EjemploParamsController.java
@@ -2,6 +2,11 @@ package com.udemycourse.springboot.web.app.springbootweb;
 
 
 import org.springframework.ui.Model;
+
+import java.text.NumberFormat;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +27,30 @@ public class EjemploParamsController {
     public String param(@RequestParam(name="texto", required = false) String texto, Model model) {
         model.addAttribute("titulo","Titulo de params/string");
         model.addAttribute("resultado","El texto enviado es: "+texto);
+        
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo,@RequestParam Integer numero, Model model) {
+        model.addAttribute("titulo","Titulo de params/string");
+        model.addAttribute("resultado","El saludo es: '"+saludo+"'y el número es'"+numero+ "'");
+        
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model) {
+        String saludo  = request.getParameter("saludo");
+        Integer numero = null;
+        try {
+            numero = Integer.parseInt(request.getParameter("numero"));
+        } catch (NumberFormatException e) {
+            // TODO: handle exception
+        }
+        
+        model.addAttribute("titulo","Titulo de params/string");
+        model.addAttribute("resultado","El saludo es: '"+saludo+"'y el número es'"+numero+ "'");
         
         return "params/ver";
     }
